c2455efed0d2bc9120c8f115e797c876a87e4bdd
John Munson <johncmunson91@gmail.com>
2020-09-13 21:35:26 -0500

    incremental update

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,12 +1,4 @@
-import { IsString } from "class-validator";
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
 
-export class UpdateCoffeeDto {
-  @IsString()
-  readonly name?: string;
-
-  @IsString()
-  readonly brand?: string;
-
-  @IsString({ each: true })
-  readonly flavors?: string[];
-}
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
