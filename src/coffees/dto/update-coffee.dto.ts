14d7dc6ce229c4f9f4c981ec240b6715ea300fd7
John Munson <johncmunson91@gmail.com>
2020-09-13 21:30:56 -0500

    incremental update

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from "class-validator";
+
 export class UpdateCoffeeDto {
+  @IsString()
   readonly name?: string;
+
+  @IsString()
   readonly brand?: string;
+
+  @IsString({ each: true })
   readonly flavors?: string[];
 }
